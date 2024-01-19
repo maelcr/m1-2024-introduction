034cf0047efd7cd0cabcfea05e9eac4f50be9cdc
creismeas Maël <mael.creismeas@outlook.fr>
2024-01-19 17:00:19 +0100

    Update exercices.py

--- exercices.py
+++ exercices.py
@@ -1,5 +1,6 @@
 import re
 import random
+import keyboard
 
 """
 Question 01:
@@ -120,6 +121,7 @@ La fonction doit être capable de générer un nombre n de mots de passes confor
 Ex: PwdGen(3) --> "?N21m8O,@y9RJ8J,@c2d01I"
 """
 def PwdGen(n=100):
+    #sinon possibilité de passer par ascii"
     lettre_min=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","w","x","y","z"]
     carac_spe=["?","#","@"]
     i=0
@@ -158,15 +160,38 @@ Question 06:
 Un robot se déplace dans un repère 2D. L'utilisateur peut le commander à l'aide des flèches directionnelles, déplacant le robot d'une case à chaque fois.
 Calculer, en temps réel, la distance entre l'origine et le robot (en nb de cases). Le robot peut se trouver à des coordonnées négatives.
 Utiliser la librairie keyboard pour détecter les touches du clavier.
-"""
+""" 
+    #correction prof
 def RcRobot():
-    pass
+    pos = [0, 0]
+    while True:
+        if keyboard.is_pressed("UP"):
+            pos[1] += 1
+            print(f"Moving Up. I'm {abs(pos[0]) + abs(pos[1])}m away from origin.")
+            while keyboard.is_pressed("UP"):
+                pass
+        elif keyboard.is_pressed("DOWN"):
+            pos[1] -= 1
+            print(f"Moving Down. I'm {abs(pos[0]) + abs(pos[1])}m away from origin.")
+            while keyboard.is_pressed("DOWN"):
+                pass
+        elif keyboard.is_pressed("LEFT"):
+            pos[0] -= 1
+            print(f"Moving Left. I'm {abs(pos[0]) + abs(pos[1])}m away from origin.")
+            while keyboard.is_pressed("LEFT"):
+                pass
+        elif keyboard.is_pressed("RIGHT"):
+            pos[0] += 1
+            print(f"Moving Right. I'm {abs(pos[0]) + abs(pos[1])}m away from origin.")
+            while keyboard.is_pressed("RIGHT"):
+                pass
 
 #print(DivisibleCheck())
 #print(SquaredNumbers())
 #print(SquaredInterval())
 #liste_100_code="?N21m8O,@y9RJ8J,@c2d01I,@4Eli0p,@qRktu6,#031uwcB,?955XT71c,@heD1P6,?nobQCoVUB9,?Lla13K,#Z4243r,@vSfyupZdC3oi,?R41gGo,?4wB2n1,?Z00Qb0,@080N0x,@EBB9RKk,#wAHk0y,?RLS21l,#Sm2pYC,?2D3ToV,?4py698b0I,BzRBsh8,?r3QJm4,?tDdoyOoqs8,@uZ39Y8,@7uY4c0,@MPWvq2,#Gy8dnb,#zbd2L0,?9m7uSF,?S2fcx1,?pmi9yG,#fP21Wj,@ljZGp5,#hW0d5x,@TqtunAX8,@d8o366vyL,@Fh516S,?Icy0Zz,#4fcwju5f4U,?ZzW2m2,?6xvtt4v3yF,@8D3P81FKf,?eOCdnb6,#lxZ8o7,@caFhwcFUx2,#995gM5,#qEDqsVD,?9W8dd7252f,@lhxz68pC,?7y78bV,#rBCJy3,@cLFrvdF4,@hmL34L,?6bZfX8,@i5T64J,@8S9WI74N4b,@0iCGOo,#hCucluCV5,?TG48zz,?i6BEk0,@2B9Vx2,#tMpRtfLMW6,@cHNMgt6,#5nW7U7,?7d811yG,@3yfJxS,?3pxZuY,?RU0WdT,#W6u253,@iu6SLD,?RGFw78,#17Eq5I,?7s8fvv6iY,#6edQnR,@wl7oH4,#0RKcam,?BBMtd5,?n6E3dN,#pIbrDJqt8,@5N3y02,@vr16gz,?650b0I,@933svJ,#29h6,#m462EB,#NyL2J1,?x3eOek,?1f14xaO,@RHnrrAVXJ8,@Gu0h31,?ODsSJlssj1,?72zD89,@yK4VCV,?UCex5Q,#4573d72aL,@J794T0A00d,@yvTSh6,#Oh5pxz"
 #print(PwdCheck(liste_100_code))
-liste_n_code=PwdGen(100)
+#liste_n_code=PwdGen(100)
 #print(liste_n_code)
-print(PwdCheck2(liste_n_code))
+#print(PwdCheck2(liste_n_code))
+RcRobot()
